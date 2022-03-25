1bf3831a91a05c46e175dd5d0e28331a24651810
Zack Plauché <zackknowspython@gmail.com>
2022-03-25 13:48:37 +0200

    Setup vite + vue scaffolding and create navbar + homepage design

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-slate-900 px-5 h-[60px]">
+    <div class="max-w-5xl mx-auto flex justify-between items-center h-full">
+      <div class="text-4xl font-semibold align-middle">
+        <span class="text-yellow-300">Auto</span><span class="text-blue-500">SA</span>
+        <span class="text-lg text-white inline-block ml-1 font-light">Auto Situational Awareness</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
