584d331360622b73716d654991cf6393b6e5cf4f
Zack Plauché <zackknowspython@gmail.com>
2022-03-25 17:44:33 +0200

    Add api client fetcher

---
+++ src/api/client.js
@@ -0,0 +1,35 @@
+import { axios } from 'axios'
+
+const developmentURL = 'http://localhost:3000/'
+const productionURL = 'https://api.example.com/'
+
+function axiosClient(baseURL) {
+  let axiosInstance = axios.create({
+    baseURL: baseURL,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+
+  return {
+    get(url, params) {
+      return axiosInstance.get(url, { params: params }).then(response => response.data).catch(error => console.log(error))
+    },
+    post(url, data) {
+      return axiosInstance.post(url, data).then(response => response.data).catch(error => console.log(error))
+    },
+    put(url, data) {
+      return axiosInstance.put(url, data).then(response => response.data).catch(error => console.log(error))
+    },
+    delete(url) {
+      return axiosInstance.delete(url).then(response => response.data).catch(error => console.log(error))
+    },
+    patch(url, data) {
+      return axiosInstance.patch(url, data).then(response => response.data).catch(error => console.log(error))
+    },
+  }
+}
+
+const apiClient = axiosClient(productionURL)
+
