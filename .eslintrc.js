08d35d9259bf5635810f2c4b71ca16a217b84a1e
Zack Plauché <zackknowspython@gmail.com>
2022-03-28 01:34:04 +0300

    Update eslint and add default background colors to home page

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     "vue/max-attributes-per-line": "off",
     "vue/singleline-html-element-content-newline": "off",
     "vue/multiline-html-element-content-newline": "off",
+    "vue/html-self-closing": "off",
   }
 }
